8d95b821f7a8493a9acb599a8f004f2b171d6d46
EduardaChiarelli <166647190+EduardaChiarelli@users.noreply.github.com>
2024-05-09 18:02:14 -0300

    Create ContaCorrente.kt

---
+++ ContaCorrente.kt
@@ -0,0 +1,14 @@
+class ContaCorrente(val numeroDaConta: Int, var saldo: Double){
+    fun imprimir(){
+        println("Conta: $numeroDaConta \nSaldo: $saldo")
+    }
+    
+    fun depositar(valor: Double){
+        saldo = saldo + valor
+    }
+    
+    fun sacar(valor: Double){
+        saldo = saldo - valor
+    }
+    
+}
